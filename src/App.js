3325b97c299d0a0f15b9eee9cd144e9c5e97c6b7
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-12-05 16:10:58 -0600

    more updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-
-import './App.css';
+import React from 'react';
+import About from './components/About';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">Who am I?</h1>
+    </section>
+  );
+}
+
+export default About;
