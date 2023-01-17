7e571464c0c7bbbf19cae56e3bfbcc86627a75c3
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2023-01-17 12:47:07 -0800

    centered buttons

--- src/components/BackCard/index.js
+++ src/components/BackCard/index.js
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function BackCard({title, text, link}) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea >
+    <Card sx={{ width: 345 }}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div" sx={{ display: 'flex', justifyContent: 'center'}}>
           {title}
@@ -18,7 +16,6 @@ export default function BackCard({title, text, link}) {
           {text}
         </Typography>
       </CardContent>
-      </CardActionArea>
       <CardActions sx={{ display: 'flex', justifyContent: 'center'}}>
         <Button href={link} size="small" sx={{ fontSize: 11, fontWeight: 'bold' }}>Github Repo</Button>
       </CardActions>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
@@ -10,23 +9,21 @@ import Typography from '@mui/material/Typography';
 
 export default function ProjectCard({img, title, text, link}) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea >
+    <Card sx={{ width: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
         image= {img}
         title="project image"
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+        <Typography gutterBottom variant="h5" component="div" sx={{ display: 'flex', justifyContent: 'center'}}>
           {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {text}
         </Typography>
       </CardContent>
-      </CardActionArea>
-      <CardActions>
+      <CardActions sx={{ display: 'flex', justifyContent: 'center'}}>
         <Button href={link} size="small" sx={{ fontSize: 11, fontWeight: 'bold' }}>Github Repo</Button>
       </CardActions>
     </Card>
